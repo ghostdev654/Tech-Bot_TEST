76067e5540c52865071933bac46406efe83c5dd9
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-14 14:07:19 -0300

    Create jadibot-serbot_button.js

---
+++ plugins/jadibot-serbot_button.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn }) => {
+    const buttons = [
+        { buttonId: '#on_code', buttonText: { displayText: '🔢 Código de 8 dígitos' }, type: 1 },
+        { buttonId: '#on_qr', buttonText: { displayText: '📸 Escanear QR' }, type: 1 }
+    ]
+
+    const buttonMessage = {
+        text: '*Selecciona una opción para conectarte como _Sub-Bot_*:\n\n> Powered by: *Tech-Bot Team*',
+        buttons: buttons,
+        headerType: 1
+    }
+
+    await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+}
+
+handler.command = ["testbtn"]
+handler.tags = ["serbot"]
+export default handler
