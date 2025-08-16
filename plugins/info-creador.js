ec96f85f24ef78a2de10c8860b2498b2963bfb1d
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-16 09:12:53 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn }) => {
-const name = 'GhostDev.js'
-const number = '5491151545427' // sin @
-const email = 'clanxgamer@yahoo.com'
-const org = 'Creador de TECH-BOT'
-const note = 'Developer de bots'
+  const name = 'GhostDev.js'
+  const number = '5491151545427' // sin @
+  const email = 'clanxgamer@yahoo.com'
+  const org = 'Creador de TECH-BOT'
+  const note = 'Developer de bots'
 
-const vcard = `
+  const vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:${name}
@@ -15,15 +15,20 @@ EMAIL;type=EMAIL:${email}
 TEL;type=CELL;type=VOICE;waid=${number}:${number}
 NOTE:${note}
 END:VCARD
-`.trim()
+  `.trim()
 
-await conn.sendMessage(m.chat, {
-contacts: {
-displayName: name,
-contacts: [{ vcard }],
-},
-}, { quoted: m })
-await conn.sendMessage(m.chat, { text: `*Contacto del Creador\n\nSi tienes preguntas, duas o sugerenias sobre el funcionamiento de *Tech-Bot V1*, puedes contactar a mi crrador a continuación:\n\n📞 *Número:* ${number}\n👤 *Nombre* ${name}\n📩 *Correo:* ${email]\n\n> “Los bots no descansan, pero yo sí, así que no me andes mandando mensaje a las 3am porque no te voy a contestar…”\n- ${name}` }, { quoted: m })
+  // Enviar contacto
+  await conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: name,
+      contacts: [{ vcard }],
+    },
+  }, { quoted: m })
+
+  // Enviar texto adicional
+  await conn.sendMessage(m.chat, { 
+    text: `*Contacto del Creador*\n\nSi tienes preguntas, dudas o sugerencias sobre el funcionamiento de *Tech-Bot V1*, puedes contactar a mi creador a continuación:\n\n📞 *Número:* ${number}\n👤 *Nombre:* ${name}\n📩 *Correo:* ${email}\n\n> “Los bots no descansan, pero yo sí, así que no me andes mandando mensaje a las 3am porque no te voy a contestar…”\n- ${name}` 
+  }, { quoted: m })
 }
 
 handler.help = ['creador']
