b822506d4fd0cd3963d2121e1ccc8a25752ae052
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-16 08:45:13 -0300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
   const name = 'GhostDev.js'
   const number = '5491151545427' // sin @
-  const email = ''
+  const email = 'clanxgamer@yahoo.com'
   const org = 'Creador de TECH-BOT'
-  const note = ''
+  const note = 'Developer de bots'
 
   const vcard = `
 BEGIN:VCARD
