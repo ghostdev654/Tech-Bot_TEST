adb5d1d43a81955836d77fa22c16dd35e5f382c2
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-26 13:30:33 -0300

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,12 +1,31 @@
 import axios from 'axios'
+import fs from 'fs'
+const premiumFile = './json/premium.json'
+
+// Aseguramos archivo
+if (!fs.existsSync(premiumFile)) fs.writeFileSync(premiumFile, JSON.stringify([]), 'utf-8')
+
+// Función de verificación
+function isBotPremium(conn) {
+  try {
+    let data = JSON.parse(fs.readFileSync(premiumFile))
+    let botId = conn?.user?.id?.split(':')[0] // extraemos el numérico del JID
+    return data.includes(botId)
+  } catch {
+    return false
+  }
+}
 
 let handler = async (m, { conn, text }) => {
+  if (!isBotPremium(conn)) {
+    return m.reply('⚠️ *Se necesita que el bot sea premium.*\n> Usa *_.buyprem_* para activarlo.')
+  }
   if (!text) return m.reply('📎 *Por favor ingresa un enlace de Mediafire*')
-  if (!/^https?:\/\/.*mediafire\.com/.test(text)) return m.reply('❗ Ingresa un enlace válido de *Mediafire*')
+  if (!/^https?:\/\/.*mediafire\.com/.test(text)) return m.reply('⚠️ Ingresa un enlace válido de *Mediafire*')
 
   try {
     // Reacciona con el reloj mientras procesa
-    await conn.sendMessage(m.chat, { react: { text: '🕓', key: m.key } })
+    await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } })
 
     const apiUrl = `https://delirius-apiofc.vercel.app/download/mediafire?url=${encodeURIComponent(text)}`
     const res = await axios.get(apiUrl)
