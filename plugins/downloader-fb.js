e8687e3f5adf70bb2729ff61ce6a6ac75060e248
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-26 14:15:35 -0300

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,8 +1,27 @@
 import fetch from 'node-fetch'
+import fs from 'fs'
+const premiumFile = './json/premium.json'
+
+// Aseguramos archivo
+if (!fs.existsSync(premiumFile)) fs.writeFileSync(premiumFile, JSON.stringify([]), 'utf-8')
+
+// Función de verificación
+function isBotPremium(conn) {
+  try {
+    let data = JSON.parse(fs.readFileSync(premiumFile))
+    let botId = conn?.user?.id?.split(':')[0] // extraemos el numérico del JID
+    return data.includes(botId)
+  } catch {
+    return false
+  }
+}
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!isBotPremium(conn)) {
+    return m.reply('⚠️ *Se necesita que el bot sea premium.*\n> Usa *_.buyprem_* para activarlo.')
+  }
   if (!args[0]) return m.reply(
-    `📥 Uso correcto:
+    `⚠️ Uso correcto:
 ${usedPrefix + command} <enlace válido de Facebook>
 
 Ejemplo:
@@ -10,7 +29,7 @@ ${usedPrefix + command} https://www.facebook.com/watch/?v=1234567890`
   )
 
   try {
-    await conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key } })
+    await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } })
 
     let api = `https://api.dorratz.com/fbvideo?url=${encodeURIComponent(args[0])}`
     let res = await fetch(api)
