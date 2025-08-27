5249c42544b3c0d6c4108aafe7aeb43af84acf0b
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-27 17:23:35 -0300

    Update tools-shazam.js

--- plugins/tools-shazam.js
+++ plugins/tools-shazam.js
@@ -14,12 +14,12 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (status.code !== 0) throw status.msg 
   let { title, artists, album, genres, release_date } = metadata.music[0]
   let txt = '╭─⬣「 *Whatmusic Tool* 」⬣\n'
-      txt += `│  ≡◦ *Titulo ∙* ${title}${artists ? `\n│  ≡◦ *Artista ∙* ${artists.map(v => v.name).join(', ')}` : ''}`
-      txt += `${album ? `\n│  ≡◦ *Album ∙* ${album.name}` : ''}${genres ? `\n│  ≡◦ *Genero ∙* ${genres.map(v => v.name).join(', ')}` : ''}\n`
-      txt += `│  ≡◦ *Fecha de lanzamiento ∙* ${release_date}\n`
+      txt += `│  ✏️ *Titulo:* ${title}${artists ? `\n│  👤 *Artista:* ${artists.map(v => v.name).join(', ')}` : ''}`
+      txt += `${album ? `\n│  📂 *Album:* ${album.name}` : ''}${genres ? `\n│  📀 *Genero:* ${genres.map(v => v.name).join(', ')}` : ''}\n`
+      txt += `│  📆 *Fecha de lanzamiento:* ${release_date}\n`
       txt += `╰─⬣`
      conn.reply(m.chat, txt, m)
-  } else return conn.reply(m.chat, `Etiqueta un audio o video de poca duración con el comando *${usedPrefix + command}* para ver que música contiene.`, m)
+  } else return conn.reply(m.chat, `✳️ Etiqueta  un audio o video de poca duración con el comando \`${usedPrefix + command}\` para ver que música contiene.`, m)
 }
 handler.help = ['whatmusic <audio/video>']
 handler.tags = ['tools']
