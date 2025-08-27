e75ce1fd0360a2787d6b29ca0384913a3f0f67cb
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-27 16:43:57 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,9 +1,9 @@
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
-await m.react('🕓')
+await m.react('⏳')
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-await conn.reply(m.chat, stdout.toString(), m, rcanal)
+await conn.reply(m.chat, stdout.toString(), m)
 await m.react('✅')
 }}
 handler.help = ['update']
