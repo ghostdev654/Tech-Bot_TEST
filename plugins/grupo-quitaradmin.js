d0bfa99c4de11e51523ba0fa7d559f26623545ae
ghostdev654 <ghostdev.js@yahoo.com>
2025-08-29 09:27:17 -0300

    Create grupo-quitaradmin.js

---
+++ plugins/grupo-quitaradmin.js
@@ -0,0 +1,29 @@
+let handler = async (m, { conn, participants, command, text }) => {
+  if (!m.isGroup) return m.reply("📌 Este comando solo funciona en grupos.")
+  if (!text && !m.mentionedJid[0] && !m.quoted) 
+    return m.reply(`⚠️ Usa: .${command} @user / reply`)
+
+  // obtener el user
+  let user = m.mentionedJid[0] 
+    ? m.mentionedJid[0] 
+    : m.quoted 
+      ? m.quoted.sender 
+      : text.replace(/[^0-9]/g, '') + "@s.whatsapp.net"
+
+  try {
+    await conn.groupParticipantsUpdate(m.chat, [user], "demote")
+    m.reply(`⏬ Usuario @${user.split('@')[0]} fue degradado a *miembro*.`, null, { mentions: [user] })
+  } catch (e) {
+    m.reply("❌ No pude degradar al usuario, revisa permisos.")
+    console.log(e)
+  }
+}
+
+handler.help = ["demote @user"]
+handler.tags = ["group"]
+handler.command = /^demote$/i
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
+
+export default handler
